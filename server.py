16d8bd4a92a3d839c9940886669f1c3e3dbcc55f
Wonho <42860714+GoldenCorgi@users.noreply.github.com>
2021-06-08 23:47:24 +0800

    Update server.py

--- server.py
+++ server.py
@@ -34,4 +34,4 @@ def hardware_push_data(data: Optional[dict] = None):
 port = int(os.getenv('PORT', 8000))
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=port, log_level="info")
+    uvicorn.run("server:app", host="0.0.0.0", port=port, log_level="info")
